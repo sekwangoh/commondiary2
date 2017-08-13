f64bcc83d4462e672f6354413e240ec7fc44e8aa
sekwangoh <osktr5@gmail.com>
2017-08-13 22:01:33 +0900

    commondiary2 first

---
+++ db/migrate/20170727153051_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.references :user, foreign_key: true
+      t.string :title
+      t.text :plan
+      t.text :research
+      t.text :act
+      t.text :observe
+      t.text :evaluate
+      t.text :reflect
+
+      t.timestamps
+    end
+  end
+end
